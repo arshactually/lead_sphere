09feac60ca7ae199c45f3bf154177fc0a37b8169
arshactually <arshactuallyfit@gmail.com>
2023-09-06 15:31:42 +0200

    Removed Build errors

--- components/About.tsx
+++ components/About.tsx
@@ -1,52 +1,102 @@
 "use client"
-import React from 'react'
-import { motion } from "framer-motion"
+// import React from 'react'
+// import { motion } from "framer-motion"
 
-type Props = {}
+// type Props = {}
+
+// export default function About({}: Props) {
+//   return (
+//     <motion.div 
+//     initial={{ opacity : 0}}
+//     whileInView={{ opacity:1}}
+//     transition={{ duration:1.5}}
+//     className='flex flex-col relative h-screen text-center md:text-left
+//     md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+
+//         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+//             About</h3>
+
+//             <motion.img 
+//             initial={{
+//                 x:-200,
+//                 opacity:0,
+//             }}
+//             whileInView={{
+//                 x:0,
+//                 opacity:1,
+//             }}
+//             viewport={{ once:true}}
+//             transition= {{
+//                 duration: 1.2,
+//             }}
+//             src='https://i.pinimg.com/564x/e1/b3/01/e1b3016a1a46ff49dc28661fc5da6970.jpg'
+
+//             className='-md-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[200px] xl:h-[200px]'
+//             />
+
+//             <div className='space-y-10 px-0 md:px-10'>
+//                 <h1 className='text-4xl font-semibold'>Here is a
+//                 <span className='underline decoration-[#005a60]/50'> little  </span> background
+//                 </h1>
+//                 <p className='text-base '>
+//                 Your end-to-end partner for AI-powered transformations, from ideation to implementation and beyond. 
+//                 We're not just automation experts; we're your dedicated team, assisting startups and enterprises in crafting groundbreaking AI solutions that streamline operations and fuel progress. 
+//                 Guided by our founder's vision, we navigate the entire journey, transforming your concepts into practical, efficiency-boosting solutions. 
+//                 With a wealth of experience spanning various sectors, we're here to construct an innovative product that propels your company towards enduring success.
+//                 </p>
+//             </div>
+    
+    
+//     </motion.div>
+//   )
+// }
+import React from 'react';
+import { motion } from 'framer-motion';
+
+type Props = {};
 
 export default function About({}: Props) {
   return (
-    <motion.div 
-    initial={{ opacity : 0}}
-    whileInView={{ opacity:1}}
-    transition={{ duration:1.5}}
-    className='flex flex-col relative h-screen text-center md:text-left
-    md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
 
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-            About</h3>
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        whileInView={{
+          x: 0,
+          opacity: 1,
+        }}
+        viewport={{ once: true }}
+        transition={{
+          duration: 1.2,
+        }}
+        src="https://i.pinimg.com/564x/e1/b3/01/e1b3016a1a46ff49dc28661fc5da6970.jpg"
+        className="-md-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[200px] xl:h-[200px]"
+        alt="Image Description"
+      />
 
-            <motion.img 
-            initial={{
-                x:-200,
-                opacity:0,
-            }}
-            whileInView={{
-                x:0,
-                opacity:1,
-            }}
-            viewport={{ once:true}}
-            transition= {{
-                duration: 1.2,
-            }}
-            src='https://i.pinimg.com/564x/e1/b3/01/e1b3016a1a46ff49dc28661fc5da6970.jpg'
-
-            className='-md-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[200px] xl:h-[200px]'
-            />
-
-            <div className='space-y-10 px-0 md:px-10'>
-                <h1 className='text-4xl font-semibold'>Here is a
-                <span className='underline decoration-[#005a60]/50'> little  </span> background
-                </h1>
-                <p className='text-base '>
-                Your end-to-end partner for AI-powered transformations, from ideation to implementation and beyond. 
-                We're not just automation experts; we're your dedicated team, assisting startups and enterprises in crafting groundbreaking AI solutions that streamline operations and fuel progress. 
-                Guided by our founder's vision, we navigate the entire journey, transforming your concepts into practical, efficiency-boosting solutions. 
-                With a wealth of experience spanning various sectors, we're here to construct an innovative product that propels your company towards enduring success.
-                </p>
-            </div>
-    
-    
+      <div className="space-y-10 px-0 md:px-10">
+        <h1 className="text-4xl font-semibold">
+          Here is a
+          <span className="underline decoration-[#005a60]/50"> little </span> background
+        </h1>
+        <p className="text-base ">
+          Your end-to-end partner for AI-powered transformations, from ideation to implementation and beyond.
+          We&apos;re not just automation experts; we&apos;re your dedicated team, assisting startups and enterprises in crafting groundbreaking AI solutions that streamline operations and fuel progress.
+          Guided by our founder&apos;s vision, we navigate the entire journey, transforming your concepts into practical, efficiency-boosting solutions.
+          With a wealth of experience spanning various sectors, we&apos;re here to construct an innovative product that propels your company towards enduring success.
+        </p>
+      </div>
     </motion.div>
-  )
+  );
 }
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,67 +1,158 @@
 "use client"
-import { PhoneIcon } from '@heroicons/react/20/solid'
-import { EnvelopeIcon } from '@heroicons/react/24/outline'
-import { MapPinIcon } from '@heroicons/react/24/solid'
-import React from 'react'
-import { useForm , SubmitHandler } from 'react-hook-form'
+// import { PhoneIcon } from '@heroicons/react/20/solid'
+// import { EnvelopeIcon } from '@heroicons/react/24/outline'
+// import { MapPinIcon } from '@heroicons/react/24/solid'
+// import React from 'react'
+// import { useForm , SubmitHandler } from 'react-hook-form'
+
+// type Inputs = {
+//     name: string
+//     email: string
+//     subject: string
+//     message: string
+//   }
+// type Props = {};
+
+// function Contact({}: Props) {
+//     const {
+//         register,
+//         handleSubmit,
+  
+//       } = useForm<Inputs>()
+//       const onSubmit: SubmitHandler<Inputs> = (formData) => {
+//         window.location.href = `mailto:leadsphere.business@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email}) ;`
+//       }
+//   return (
+//     <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+        
+//         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+//             Contact Us</h3>
+
+//         <div className='flex flex-col space-y-10'>
+//             <h4 className='text-4xl font-semibold text-center'>
+//             We've got just what you need.{" "}
+//             <span className='decoration-[#005a60]/50 underline'>Let's Talk.</span>
+//         </h4>
+
+//             <div className=' text-[#ffff] flex items-center space-x-5 justify-center '>
+//                 <PhoneIcon className='text-[#005a60] h-7 w-7 animate-pulse'/>
+//                 <p className='text-2xl'>+4915750125829</p>
+//             </div>
+//             <div className=' text-[#ffff] flex items-center space-x-5 justify-center '>
+//                 <EnvelopeIcon className='text-[#005a60] h-7 w-7 animate-pulse'/>
+//                 <p className='text-2xl'>leadsphere.business@gmail.com</p>
+//             </div>
+//             <div className=' text-[#ffff] flex items-center space-x-5 justify-center '>
+//                 <MapPinIcon className='text-[#005a60] h-7 w-7 animate-pulse'/>
+//                 <p className='text-2xl'>Dresden, Germany</p>
+//             </div>
+//             <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto'>
+//                 <div className='flex space-x-2'>
+//                     <input {...register('name')} placeholder="Name" className="contactInput" type="text" />
+//                     <input {...register('email')} placeholder="Email" className="contactInput" type="text" />
+//                 </div>
+//                 <input {...register('subject')} placeholder="Subject" className="contactInput" type="text" />
+
+//                 <textarea {...register('message')} placeholder="Message" className="contactInput" ></textarea>
+//                 <button type="submit"
+//                 className='bg-[#005a60] py-5 px-10 rounded-md text-blacl ont-bold text-lg '>Submit</button>
+//             </form>
+            
+           
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Contact
+
+import { PhoneIcon } from '@heroicons/react/20/solid';
+import { EnvelopeIcon } from '@heroicons/react/24/outline';
+import { MapPinIcon } from '@heroicons/react/24/solid';
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 type Inputs = {
-    name: string
-    email: string
-    subject: string
-    message: string
-  }
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 type Props = {};
 
 function Contact({}: Props) {
-    const {
-        register,
-        handleSubmit,
-  
-      } = useForm<Inputs>()
-      const onSubmit: SubmitHandler<Inputs> = (formData) => {
-        window.location.href = `mailto:leadsphere.business@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email}) ;`
-      }
-  return (
-    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-            Contact Us</h3>
+  const {
+    register,
+    handleSubmit,
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:leadsphere.business@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email}) ;`;
+  };
 
-        <div className='flex flex-col space-y-10'>
-            <h4 className='text-4xl font-semibold text-center'>
-            We've got just what you need.{" "}
-            <span className='decoration-[#005a60]/50 underline'>Let's Talk.</span>
+  return (
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contact Us
+      </h3>
+
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
+          We've got just what you need.{" "}
+          <span className="decoration-[#005a60]/50 underline">Let's Talk.</span>
         </h4>
 
-            <div className=' text-[#ffff] flex items-center space-x-5 justify-center '>
-                <PhoneIcon className='text-[#005a60] h-7 w-7 animate-pulse'/>
-                <p className='text-2xl'>+4915750125829</p>
-            </div>
-            <div className=' text-[#ffff] flex items-center space-x-5 justify-center '>
-                <EnvelopeIcon className='text-[#005a60] h-7 w-7 animate-pulse'/>
-                <p className='text-2xl'>leadsphere.business@gmail.com</p>
-            </div>
-            <div className=' text-[#ffff] flex items-center space-x-5 justify-center '>
-                <MapPinIcon className='text-[#005a60] h-7 w-7 animate-pulse'/>
-                <p className='text-2xl'>Dresden, Germany</p>
-            </div>
-            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto'>
-                <div className='flex space-x-2'>
-                    <input {...register('name')} placeholder="Name" className="contactInput" type="text" />
-                    <input {...register('email')} placeholder="Email" className="contactInput" type="text" />
-                </div>
-                <input {...register('subject')} placeholder="Subject" className="contactInput" type="text" />
-
-                <textarea {...register('message')} placeholder="Message" className="contactInput" ></textarea>
-                <button type="submit"
-                className='bg-[#005a60] py-5 px-10 rounded-md text-blacl ont-bold text-lg '>Submit</button>
-            </form>
-            
-           
+        <div className="text-[#ffff] flex items-center space-x-5 justify-center">
+          <PhoneIcon className="text-[#005a60] h-7 w-7 animate-pulse" />
+          <p className="text-2xl">+4915750125829</p>
         </div>
+        <div className="text-[#ffff] flex items-center space-x-5 justify-center">
+          <EnvelopeIcon className="text-[#005a60] h-7 w-7 animate-pulse" />
+          <p className="text-2xl">leadsphere.business@gmail.com</p>
+        </div>
+        <div className="text-[#ffff] flex items-center space-x-5 justify-center">
+          <MapPinIcon className="text-[#005a60] h-7 w-7 animate-pulse" />
+          <p className="text-2xl">Dresden, Germany</p>
+        </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto"
+        >
+          <div className="flex space-x-2">
+            <input
+              {...register('name')}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register('email')}
+              placeholder="Email"
+              className="contactInput"
+              type="text"
+            />
+          </div>
+          <input
+            {...register('subject')}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+
+          <textarea
+            {...register('message')}
+            placeholder="Message"
+            className="contactInput"
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-[#005a60] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
