1835c208df42bd8116560e23cb1a1124c95d997c
arshactually <arshactuallyfit@gmail.com>
2023-09-07 15:16:22 +0200

    added botpress chatbot

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,6 +33,11 @@ function Footer({}: Props) {
 
     </div>
 
+    <div>
+        <script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
+        <script src="https://mediafiles.botpress.cloud/5a418cc2-b33f-43ca-a21e-11478d8644ee/webchat/config.js" defer></script>
+    </div>
+
 
     <div className=' font-semibold px-10'>
         <h3>Built with love by Arshactually™</h3>
