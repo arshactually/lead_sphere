6a548db4a4b5a11c31f596a19cd5fd3c30f38d57
arshactually <arshactuallyfit@gmail.com>
2023-09-07 14:14:29 +0200

    made some changes

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -1,42 +1,34 @@
-import React from 'react'
-import { motion } from "framer-motion"
+import React from 'react';
+import { motion } from 'framer-motion';
 
-type Props = {}
+type Props = {};
 
 export default function WorkCard({}: Props) {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0
-    
-w-[500px] h-[900px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
-
-        <motion.div 
-        initial={{ y:-100,
-            opacity : 0}}
-        
-        whileInView={{ y:0 , opacity:1}}
-        transition={{ duration:1.2}}
-        viewport={{ once:true}}
+    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] h-[900px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+      <motion.img
+        initial={{ y: -100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1.2 }}
+        viewport={{ once: true }}
         className='relative rounded-full h-32 w-32 mx-auto object-cover'
-        src="" alt="" />
+        src='https://i.postimg.cc/Y2vdvFwy/icon.png' // Replace with your image source URL
+        alt='alternative-text' // Replace with your alternative text
+      />
 
-        <div className='px-0 md:px-10'>
-            <h4 className='text-4xl font-light'>Chatbot Integration</h4>
-            <p className='font-bold text-2xl mt-1'>AI- Automation</p>
-            <div className='flex space-x-2 my-2'>
-                
-
-                
-
-            </div>
-            <p className='uppercase py-5 text-gray-500'>Started work.. - Ended...</p>
-
-            <ul className='list-disc space-y-4 ml-5 text-lg'>
-                <li>Summary points</li>
-                <li>Summary points</li>
-
-            </ul>
+      <div className='px-0 md:px-10'>
+        <h4 className='text-4xl font-light'>Chatbot Integration</h4>
+        <p className='font-bold text-2xl mt-1'>AI- Automation</p>
+        <div className='flex space-x-2 my-2'>
+          {/* Content goes here */}
         </div>
+        <p className='uppercase py-5 text-gray-500'>Started work.. - Ended...</p>
 
+        <ul className='list-disc space-y-4 ml-5 text-lg'>
+          <li>Summary points</li>
+          <li>Summary points</li>
+        </ul>
+      </div>
     </article>
-  )
+  );
 }
